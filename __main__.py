90f510a085e0f593a8b629bfd8f3a40b7875a6d8
Alex Gospodchikov <aleks-say@mail.ru>
2017-11-30 16:05:15 +0300

    Second version of interface.

--- __main__.py
+++ __main__.py
@@ -6,11 +6,11 @@ def interface():
     while not done:
         print("Queries:")
         print("=========")
-        print("0.Forgotten bag in car with AN (query 1)")
-        print("1.Percent of busy taxis (query 3)")
-        print("2.Double charged client (query 4)")
-        print("3.Least used taxis (query 7)")
-        print("4.")
+        print("0. Forgotten bag in car with AN (query 1)")
+        print("1. Percent of busy taxis (query 3)")
+        print("2. Double charged client (query 4)")
+        print("3. Least used taxis (query 7)")
+        print("4. Research on location of residence (query 8)")
         choice = input("Enter the number of the required query (0-4): ")
         try:
             choice = int(choice)
@@ -27,6 +27,7 @@ def interface():
 def execute(choice):
     query = str()
     title = str()
+    variables = dict()
     if choice == 0:
         title = "Potential car IDs for customer_id 371 on 28.11.2017 (\"today\"):"
         query = """
@@ -35,6 +36,16 @@ def execute(choice):
         LEFT JOIN Car ON Car.car_id = Ordering.car_id
         WHERE Ordering.date = "2017-11-28" AND Ordering.customer_id = 371 AND Car.color = "red" AND Car.plate_series LIKE "AN%";
         """
+    elif choice == 2:
+        title = "IDs of double charged orders for user 371"
+        query = """
+            SELECT Payment.order_id
+            FROM Payment
+            LEFT JOIN Ordering ON Ordering.order_id = Payment.order_id
+            WHERE Ordering.customer_id = 371 AND Ordering.date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()
+            GROUP BY Payment.order_id
+            HAVING COUNT(Payment.order_id) > 1;
+            """
     elif choice == 3:
         title = "IDs of 10% of least used cars:"
         query = """
@@ -51,26 +62,45 @@ def execute(choice):
         EXECUTE STMT USING @totalCars;
         """
     elif choice == 4:
-        title = "IDs of double charged orders for user 371"
+        done = False
+        inp = str()
+        while not done:
+            inp = str(input("Please enter a date in form of YYYY-MM-DD: "))
+            done = True
+
+        variables["input"] = inp
+        title = "Research results:"
         query = """
-        SELECT Payment.order_id
-        FROM Payment
-        LEFT JOIN Ordering ON Ordering.order_id = Payment.order_id
-        WHERE Ordering.customer_id = 371 AND Ordering.date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()
-        GROUP BY Payment.order_id
-        HAVING COUNT(Payment.order_id) > 1;
+        SELECT DISTINCT O.customer_id AS UserId, COUNT(C.charge_id) AS Amount
+        FROM Ordering O, Charge C
+        WHERE O.car_id = C.car_id 
+        AND %(input)s <= O.date
+        AND DATE_ADD(%(input)s, INTERVAL 1 MONTH) >= O.date 
+        AND O.date = C.date
+        GROUP BY O.customer_id;
         """
     db = MySQLdb.connect("localhost", "root", "mysql")
     x = db.cursor()
     queries = query.split(";")[:-1]
 
+    if len(variables) == 0:
+        variables = None
+
     x.execute("USE InnoProject3;")
     for c in range(len(queries) - 1):
-        x.execute(queries[c])
-    x.execute(queries[-1])
+        x.execute(queries[c], variables)
+    x.execute(queries[-1], variables)
     print(title)
+    out = str()
+    for name in [i[0] for i in x.description]:
+        out += name + " "
+    print(out)
     for c in range(x.rowcount):
-        print(x.fetchone()[0])
+        row = x.fetchone()
+        out = str()
+        for c2 in range(len(row)):
+            out += str(row[c2]) + " "
+        print(out)
 
     db.close()
 
